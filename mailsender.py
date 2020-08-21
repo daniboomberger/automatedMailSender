6f6fbb171c8c32d790dcb66cc8623786405c5b58
ACMT\D.Jakupi <d.jakupi@acommit.ch>
2020-08-21 13:24:35 +0200

    cleaned up the code and made it working

--- mailsender.py
+++ mailsender.py
@@ -7,6 +7,10 @@ from email import encoders
 import json
 import smtplib
 
+'''
+ TODO: add error handling
+'''
+
 #variables for user credentials
 user_email = ''
 user_password = ''
@@ -28,15 +32,20 @@ mail_body = ''
 
 #function to provide connection to mail server and also sending the actual message
 def mailServerConnection() :
-    mail_server = smtplib.SMTP_SSL(smtp_url, port)
+    mail_server = smtplib.SMTP_SSL(smtp_url, smtp_port)
     mail_server.ehlo()
     mail_server.login(user_email, user_password)
     mail_server.send_message(mail_message)
 
 #function to parse the json file
-def jsonParser(): 
-    json_data = json.loads(json_path)
-    for i in range(0, 100):
+def jsonParser():
+    global mail_subject
+    global mail_receiver
+    global mail_body
+
+    with open(json_path, 'r') as json_file:
+        json_data = json.load(json_file)
+    for i in range(0, len(json_data)-1):
         current_json_data = json_data[str(i)]
         mail_subject = current_json_data['subject']
         mail_receiver = current_json_data['receiver']
@@ -48,30 +57,31 @@ def jsonParser():
 #function which creates with an template and json data a mail body
 def createMailBody(data):
     mail_template_text = open(mail_template_path).read()
-    template =  Template(template_mail_text)
+    template = Template(mail_template_text)
 
     try:
-        mail_template = template_mail.substitute(
+        body = template.substitute(
             name = data['name'], 
             formal = data['formal'],
-            formal_message = data['formal_message']
         )
     except: 
         print('An Error occured with the template')
-        return Mail_message
+    return body 
 
 #function to create an mail message with necassary config
 def mailMessage():
     mail_message['Subject'] = mail_subject
     mail_message['FROM'] = user_email
     mail_message['To'] = mail_receiver
-    mail_message.attach(MIMEText(message, 'plain'))
+    mail_message.attach(MIMEText(mail_body, 'plain'))
 
 
-def main():
+if __name__ == '__main__':
     user_email = input('Enter your e-mail: ')
     user_password = getpass('Enter your password: ')
     smtp_url = input('Enter the smtp ssl url: ') 
     smtp_port = input('Enter the smtp ssl port: ')
     json_path = input('Enter your json file path: ')
-    mail_template = input('Enter your mail template file path: ')
+    mail_template_path  = input('Enter your mail template file path: ')
+    jsonParser()
+
